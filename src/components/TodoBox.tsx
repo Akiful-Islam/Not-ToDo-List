7f40631e47b9b219db5db2682af740500551158b
Arian Islam <arian.islam024@gmail.com>
2024-03-18 15:38:53 +0600

    Add and implement card component

--- src/components/TodoBox.tsx
+++ src/components/TodoBox.tsx
@@ -1,27 +1,27 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { PROJECT_TITLE } from "@/lib/data/projectInfo";
 import AddTodo from "./AddTodo";
 
 const TodoBox = () => {
   return (
-    <div
-      className={`h-[80vh] w-[25vw] bg-gray-300
-                m-12 py-12 px-8 rounded-lg
-                transition-all ease-linear duration-100 hover:shadow-lg hover:scale-[1.01]`}
-    >
-      <div
-        className={`mb-6 flex justify-between items-center
-      `}
-      >
-        <h1 className="text-4xl font-extrabold font-serif tracking-tight lg:text-5xl">
-          {PROJECT_TITLE}
-        </h1>
+    <Card>
+      <CardHeader>
+        <CardTitle>{PROJECT_TITLE}</CardTitle>
+        <CardDescription>Your Not To Do List</CardDescription>
         <AddTodo
           buttonLabel="Add New"
           dialogHeader="Add A New not To Do"
           dialogDescription="Don't do it!"
         />
-      </div>
-    </div>
+      </CardHeader>
+      <CardContent></CardContent>
+    </Card>
   );
 };
 
