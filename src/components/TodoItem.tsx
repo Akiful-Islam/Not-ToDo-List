3f048fef649e512cee43b40dae9877a769dbcab0
Arian Islam <arian.islam024@gmail.com>
2024-03-19 17:34:06 +0600

    fix unused imports

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -9,7 +9,6 @@ import {
   DialogHeader,
   DialogFooter,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
