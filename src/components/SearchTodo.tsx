1cc76764151253c06ee3123fdd9400b855c3f336
Arian Islam <arian.islam024@gmail.com>
2024-03-24 16:13:27 +0600

    create and implement search todo component

---
+++ src/components/SearchTodo.tsx
@@ -0,0 +1,49 @@
+import { Filter } from "@/lib/data/types";
+import React from "react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Search } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+type Props = {
+  filter: Filter;
+  setFilter: (filter: Filter) => void;
+};
+
+const SearchTodo: React.FC<Props> = ({ filter, setFilter }) => {
+  const onSearch = (searchTerm: string) => {
+    setFilter({
+      ...filter,
+      searchTerm,
+    });
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Search
+          className="text-popover-foreground txt hover:bg-accent rounded-md transition-all duration-150 ease-out"
+          size="1.25rem"
+        />
+      </PopoverTrigger>
+      <PopoverContent>
+        <div className="flex items-center gap-4">
+          <Label htmlFor="search-input">
+            <Search size="1.25rem" className="text-input" />
+          </Label>
+          <Input
+            id="search-input"
+            placeholder="Search To Don'ts"
+            onChange={(e) => onSearch(e.target.value)}
+          />
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default SearchTodo;
